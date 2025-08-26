cdad5599dc0ed2f49234bb510a5bad5ae8256f1b
Felipe Barbosa <felipe2016depaula@gmail.com>
2025-08-26 08:13:06 -0300

    feat: Coloca o Lombok na classse para abstração

--- src/main/java/Springboot/CadastroNinja/Ninjas/NinjaModel.java
+++ src/main/java/Springboot/CadastroNinja/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
 package Springboot.CadastroNinja.Ninjas;
 import Springboot.CadastroNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -8,6 +11,9 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -24,32 +30,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Foring key ou chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
 
 }
