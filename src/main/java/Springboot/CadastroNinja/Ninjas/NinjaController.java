927d8510101c1f7c5f82a537ddd1d2b32e944687
Felipe Barbosa <felipe2016depaula@gmail.com>
2025-10-01 10:48:06 -0300

    feat: Atualiza o controller com o mack de dados

--- src/main/java/Springboot/CadastroNinja/Ninjas/NinjaController.java
+++ src/main/java/Springboot/CadastroNinja/Ninjas/NinjaController.java
@@ -1,13 +1,43 @@
 package Springboot.CadastroNinja.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
-    @GetMapping("/BoasVindas") public String BoasVindas() {
+    @GetMapping("/BoasVindas")
+    public String BoasVindas() {
         return  "Essa é minha primeira menssagem nessa rota";
     }
+
+    // Adicionar Ninja (CREATE)
+    @PostMapping ("/Criar")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
+
+    // Procurar Ninja por ID (CREATE)
+    @PostMapping ("/todos")
+    public String mostrarTodos0sNinjas(){
+        return "Mostrar Ninja";
+    }
+
+    // Mostrar os Ninjas (READ)
+    @PostMapping ("/todosID")
+    public String mostrarTodos0sNinjasId(){
+        return "Mostrar Ninja Criado";
+    }
+
+    // Alterar Dados Dos Ninjas (UPDATE)
+    @PutMapping("/AlterarID")
+    public String alterarNinjaPorId(){
+        return "Alterar Ninja Por ID";
+    }
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja Deletado por Id";
+    }
+
+
 }
